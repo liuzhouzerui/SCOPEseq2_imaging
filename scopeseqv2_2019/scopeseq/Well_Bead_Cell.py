ea8bf85d2b33cca4612537c1bbcbb159cdd26c49
Zhouzerui Liu <zl2707@columbia.edu>
2019-09-25 14:27:23 -0400

    fix bead cell align

--- scopeseqv2_2019/scopeseq/Well_Bead_Cell.py
+++ scopeseqv2_2019/scopeseq/Well_Bead_Cell.py
@@ -53,16 +53,16 @@ class WellBeadCell:
         landmark_fn = fnmatch.filter(os.listdir(self.well_folder), '*UL_BR_' + str(self.n_lane) + '.csv')[0]
         landmark_all = pd.read_csv(self.well_folder + landmark_fn)
         # rotate for each small parts, parts number = landmark.shape[0]/4
-        parts_number = landmark_all.shape[0]/4
-        for i in range(int(parts_number)):
+        parts_number = int(landmark_all.shape[0]/4)
+        for i in range(parts_number):
             landmark = landmark_all.iloc[(i*4):((i+1)*4), :]
             target_vector = np.array(
                 [landmark.iloc[1, 9] - landmark.iloc[0, 9], landmark.iloc[1, 10] - landmark.iloc[0, 10]])
             initial_vector = np.array(
                 [landmark.iloc[3, 9] - landmark.iloc[2, 9], landmark.iloc[3, 10] - landmark.iloc[2, 10]])
             self.rotation_matrix = find_rotation_matrix(target_vector, initial_vector)
-            self.bead_rotated_position[self.bead.patch == i] = np.array(landmark.loc[0, ['XM', 'YM']]) + \
-                np.dot(np.array(self.bead.bead_position - landmark.loc[2, ['XM', 'YM']]), self.rotation_matrix) * \
+            self.bead_rotated_position[self.bead.patch == i] = np.array(landmark.iloc[0, 9:11]) + \
+                np.dot(np.array(self.bead.bead_position[self.bead.patch == i] - landmark.iloc[2, 9:11]), self.rotation_matrix) * \
                 target_vector / np.dot(initial_vector, self.rotation_matrix)
 
     def link_bead(self, bead, d_th=None):
