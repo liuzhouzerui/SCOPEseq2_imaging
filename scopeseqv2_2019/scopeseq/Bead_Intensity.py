fix bug in bead position rotation

scopeseqv2_2019/scopeseq/Bead_Intensity.py
@@ -7,8 +7,7 @@ from scopeseq.method import register, assign_obc
 
 
 class BeadIntensity:
-    def __init__(self, bead_intensity_folder, channel_name, n_lane=0, total_lanes=1, total_patches_per_lane=10,
-                 round_number=8, d_th=40):
+    def __init__(self, bead_intensity_folder, channel_name, n_lane=0, total_lanes=1, total_patches_per_lane=10, round_number=8, d_th=40):
         """
 
         :param bead_intensity_folder:
@@ -28,6 +27,7 @@ class BeadIntensity:
         self.bead_position = None
         self.probe = None
         self.bg = None
+        self.borders = None
         self.patch = np.array([])
         self.obc = []
         self.obc_s = []
@@ -131,8 +131,8 @@ class BeadIntensity:
         :return:
         """
         print('assigning patches...')
-        borders = np.arange(first_border, first_border + (self.n_patch_per_lane + 1) * border_gap, border_gap)
-        self.patch = self.bead_position['XM'].apply(lambda x: np.argmax((borders > x) * 1)-1)
+        self.borders = np.arange(first_border, first_border + (self.n_patch_per_lane + 1) * border_gap, border_gap)
+        self.patch = self.bead_position['XM'].apply(lambda x: np.argmax((self.borders > x) * 1)-1)
         self.patch = self.patch + self.n_patch_per_lane * self.n_lane
 
     def obc_calling(self, barcode_ref_fn, no_signal_th=None, mode='all'):

scopeseqv2_2019/scopeseq/Well_Bead_Cell.py
@@ -54,16 +54,30 @@ class WellBeadCell:
         landmark_all = pd.read_csv(self.well_folder + landmark_fn)
         # rotate for each small parts, parts number = landmark.shape[0]/4
         parts_number = int(landmark_all.shape[0]/4)
-        for i in range(parts_number):
-            landmark = landmark_all.iloc[(i*4):((i+1)*4), :]
+        if parts_number == 1:
+            landmark = landmark_all
             target_vector = np.array(
                 [landmark.iloc[1, 9] - landmark.iloc[0, 9], landmark.iloc[1, 10] - landmark.iloc[0, 10]])
             initial_vector = np.array(
                 [landmark.iloc[3, 9] - landmark.iloc[2, 9], landmark.iloc[3, 10] - landmark.iloc[2, 10]])
             self.rotation_matrix = find_rotation_matrix(target_vector, initial_vector)
-            self.bead_rotated_position[self.bead.patch == i] = np.array(landmark.iloc[0, 9:11]) + \
-                np.dot(np.array(self.bead.bead_position[self.bead.patch == i] - landmark.iloc[2, 9:11]), self.rotation_matrix) * \
+            self.bead_rotated_position = np.array(landmark.iloc[0, 9:11]) + \
+                np.dot(np.array(self.bead.bead_position - landmark.iloc[2, 9:11]), self.rotation_matrix) * \
                 target_vector / np.dot(initial_vector, self.rotation_matrix)
+        else:
+            borders = np.arange(self.bead.borders[0], self.bead.borders[-1]+(self.bead.borders[-1]-self.bead.borders[0])
+                                / parts_number, (self.bead.borders[-1]-self.bead.borders[0])/parts_number)
+            bead_parts = self.bead.bead_position['XM'].apply(lambda x: np.argmax((borders > x) * 1) - 1)
+            for i in range(parts_number):
+                landmark = landmark_all.iloc[(i*4):((i+1)*4), :]
+                target_vector = np.array(
+                    [landmark.iloc[1, 9] - landmark.iloc[0, 9], landmark.iloc[1, 10] - landmark.iloc[0, 10]])
+                initial_vector = np.array(
+                    [landmark.iloc[3, 9] - landmark.iloc[2, 9], landmark.iloc[3, 10] - landmark.iloc[2, 10]])
+                self.rotation_matrix = find_rotation_matrix(target_vector, initial_vector)
+                self.bead_rotated_position[bead_parts == i] = np.array(landmark.iloc[0, 9:11]) + \
+                    np.dot(np.array(self.bead.bead_position[bead_parts == i] - landmark.iloc[2, 9:11]), self.rotation_matrix) * \
+                    target_vector / np.dot(initial_vector, self.rotation_matrix)
 
     def link_bead(self, bead, d_th=None):
         """